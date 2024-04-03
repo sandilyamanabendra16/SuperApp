e459d809f7338ae4199113e312304f154bff8232
sandilyamanabendra16 <81513455+sandilyamanabendra16@users.noreply.github.com>
2024-04-03 22:38:05 +0530

    reg page

--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -26,27 +26,33 @@ function handlesubmit(){
     }
 }
 return <div className={styles.page}>
-    <div >
+    <div className={styles.left}>
         <h1> Discover new things on Superapp</h1>
-        <img src={BgImg} alt='background'></img>
+        <img className={styles.bgimg} src={BgImg} alt='background'></img>
     </div>
-    <div> 
-    <div>
-        
+    <div className={styles.right}> 
+    <div className={styles.form}>
+
         <h2>Super app</h2>
-        <h6> Create your new account</h6>
+        <h3> Create your new account</h3>
+        <div>
         <input type='text' value={formdata.name} onChange={(e)=>{
             setformdata({...formdata, name: e.target.value});
         }} placeholder='Name' /> 
+        </div>
+        <br/>
         <input type='text' value={formdata.username} onChange={(e)=>{
             setformdata({...formdata, username: e.target.value})
         }} 
         placeholder="UserName" />
+        <br/>
+        <br/>
         <input type='emai' value={formdata.email} onChange={(e)=>{
             setformdata({...formdata, email: e.target.value})
         }} 
         placeholder='Email'/>
-
+        <br/>
+        <br/>
         <input type='tel' value={formdata.mobile} onChange={(e)=>{
             setformdata({...formdata, mobile: e.target.value})
         }} 
@@ -57,9 +63,11 @@ return <div className={styles.page}>
             setformdata({...formdata, checked: !formdata.checked})
         }}/>
         <label> Share my registration data with Superapp </label>
-    </div>
-    <div>
+        <br/>
         <button type='submit' onClick={handlesubmit}> SIGN UP </button>
+    </div>
+    <div className={styles.footer}>
+
         <p> By clicking on Sign up. you agree to Superapp <span>  Terms and Conditions of Use </span> </p>
         <p> To learn more about how Superapp collects, uses, shares and protects your personal data please head Superapp <span>Privacy Policy</span></p>
     </div>
